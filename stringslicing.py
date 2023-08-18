4170fafc4bbdce8680553cc2ff25a12adf092ced
rohanaragade <138466333+rohanaragade@users.noreply.github.com>
2023-08-18 11:20:10 +0530

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,26 @@
+r="rohan aragade"
+print(r[3])
+
+r="rohan aragade"
+print(r[3:])
+
+r="rohan aragade"
+print(r[:3])
+
+r="rohan aragade"
+print(r[3:-5])
+
+r="rohan aragade"
+print(r[6:13])
+
+r="rohan aragade"
+print(r[:])
+
+r="rohan aragade"
+print(r[0:])
+
+r="rohan aragade"
+print(r[-1::-1])
+
+r="rohan aragade"
+print(r[1:])
