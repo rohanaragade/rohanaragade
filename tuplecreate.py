596e210705c307e7c68851902180f499a5d78b17
rohanaragade <138466333+rohanaragade@users.noreply.github.com>
2023-08-18 11:23:05 +0530

    Add files via upload

---
+++ tuplecreate.py
@@ -0,0 +1,23 @@
+t3=tuple()
+print(t3)
+
+tup1=(1)  #  class= int
+print(type(tup1))
+
+t=(1,)    # class=tuple
+print(type(t))
+
+t1=tuple("world")
+print(t1)
+
+
+# tuple=("world")
+# print(tuple)
+
+t2=tuple([1,2,3,4,5])
+print(t2) 
+tuple=(1,2,'rohan')
+print(tuple)
+
+tup=(1,2,3,4,'rohan aragade','dypcet')
+print(tup)
